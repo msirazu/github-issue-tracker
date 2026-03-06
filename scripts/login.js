a4146d526882bcf438f32f3975f89036a6f76fb6
msirazu <thebdrazu@gmail.com>
2026-03-06 21:26:31 +0600

    main page design started

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,26 @@
-document.getElementById('form').addEventListener('submit', (e) => {
+const form = document.getElementById('form');
+const errorMsg = document.getElementById('error-msg');
+const successMsg = document.getElementById('success-msg');
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const userName = e.target.name.value;
     const password = e.target.password.value;
 
     if (userName !== 'admin') {
-        alert('username incorect');
+        errorMsg.innerText = '';
+        errorMsg.innerText = 'username incorect';
         return;
     }
 
     if (password !== 'admin123') {
-        alert('password incorect');
+        errorMsg.innerText = '';
+        errorMsg.innerText = 'password incorect';
         return;
     }
 
+    successMsg.innerText = '';
+    successMsg.innerText = 'login success';
     window.open('main.html', '_self');
 
     e.target.name.value = '';
